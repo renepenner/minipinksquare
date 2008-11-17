400da50bd5260c1f869d20300a343c2dcbdc0e5a
renepenner <renepenner@1880ae42-b4b0-11dd-818f-3bd198e1d18e>
2008-11-17 14:28:37 +0000

    Initial import.

---
+++  core/minipinksquare/config.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+define('DB_TYPE', 'MySQL');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'minipinksquare');
+
+define('PATH_LIB',      	dirname(__FILE__).'/lib/');
+define('INTERFACE_LIB',     dirname(__FILE__).'/lib/interfaces');
+define('CONTENTTYPES_LIB', 	dirname(__FILE__).'/plugins/contenttypes/');
+
+set_include_path(get_include_path() . PATH_SEPARATOR . PATH_LIB . PATH_SEPARATOR . CONTENTTYPES_LIB . PATH_SEPARATOR . INTERFACE_LIB); 
+function __autoload($class_name) {
+    require_once $class_name . '.php';
+}
+?>
